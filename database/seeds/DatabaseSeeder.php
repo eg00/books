640ce6f6b7208636028a088d7ec3a87df9d11c8a
Egor Povshok <egor@povshok.ru>
2020-08-13 01:07:09 +0500

    Seeder adds more authors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        factory(Author::class, 30)->create()
+       $authors =  factory(Author::class, 30)->create()
             ->each(static function ($author) {
                 $author->books()
                     ->sync(factory(Book::class, \random_int(1, 4))->create());
             });
+       $authors->each(function ($author){
+           $author->books()->attach(Book::inRandomOrder()->limit(\random_int(0, 4))->pluck('id'));
+       });
     }
 }
